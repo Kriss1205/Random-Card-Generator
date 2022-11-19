72ef4a2c1fc88da581ea9d0c48bc41647ca252ce
Christopher Morejon <Christophermorejon17@gmail.com>
2022-11-19 05:17:58 +0000

    Done w/ Card Gen.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,71 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function deckBuilder() {
+    const values = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+
+    const cards = [];
+    for (let s = 0; s < suits.length; s++) {
+      for (let v = 0; v < values.length; v++) {
+        const value = values[v];
+        const suit = suits[s];
+        cards.push({ value, suit });
+      }
+    }
+    return cards;
+  }
+
+  function randomCard(cards) {
+    const random = Math.floor(Math.random() * 51);
+
+    const cardValue = cards[random].value;
+
+    const cardSuit = cards[random].suit;
+
+    const cardBody =
+      cardSuit === "Diamonds" ? "&diams;" : "&" + cardSuit.toLowerCase() + ";";
+
+    const card = document.createElement("div");
+    card.classList.add("card", cardSuit.toLowerCase());
+    card.innerHTML =
+      '<span class="card-value-suit top">' +
+      cardValue +
+      cardBody +
+      "</span>" +
+      '<span class="card-suit">' +
+      cardBody +
+      "</span>" +
+      '<span class="card-value-suit bot">' +
+      cardValue +
+      cardBody +
+      "</span>";
+    document.body.appendChild(card);
+  }
+
+  setTimeout(function() {
+    location.reload();
+  }, 10000);
+
+  const cards = deckBuilder();
+  randomCard(cards);
+
+  console.log(deckBuilder());
 };
